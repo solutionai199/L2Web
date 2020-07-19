50a60fdd749c7ab80401945f3fa6eec8d89745bf
Aidan McAra <51344785+aidansbhs@users.noreply.github.com>
2020-07-19 21:06:39 +1200

    Add files via upload
    Finished verticle nav, almost done section about the overview of apocalypse.

--- code.js
+++ code.js
@@ -1,15 +1,23 @@
-var CLOSED = true;
+var CLOSED = false;
 var OPEN = false;
 
 function Nav() {
+    console.log('hi')
     if (CLOSED) {
         document.getElementById("mySidebar").style.width = "0px";
+        document.getElementById("mySidebar").style.border = '0vw solid black';
         CLOSED = false;
         OPEN = true;
+        
+        document.getElementById("second-sidebar").style.width = "0px";
 
     } else {
         document.getElementById("mySidebar").style.width = "12vw";
         CLOSED = true;
+        document.getElementById("mySidebar").style.border = '0.75vw solid black';
+        
+        document.getElementById("second-sidebar").style.width = "12vw";
     }
 
-}
+}   
+
