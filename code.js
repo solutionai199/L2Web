91ec862f3f28df5a5bc914235b617665603090dc
Aidan McAra <51344785+aidansbhs@users.noreply.github.com>
2020-07-24 13:29:57 +1200

    Add files via upload
    Doing survival guide page

--- code.js
+++ code.js
@@ -2,10 +2,11 @@ var CLOSED = false;
 var OPEN = false;
 
 function Nav() {
-    console.log('hi')
+    console.log('working?')
     if (CLOSED) {
-        document.getElementById("mySidebar").style.width = "0px";
+        document.getElementById("mySidebar").style.width = '0px';
         document.getElementById("mySidebar").style.border = '0vw solid black';
+        document.getElementById("box").style.margin = '2vw 1.25vw 1.25vw';
         CLOSED = false;
         OPEN = true;
         
@@ -15,9 +16,9 @@ function Nav() {
         document.getElementById("mySidebar").style.width = "12vw";
         CLOSED = true;
         document.getElementById("mySidebar").style.border = '0.75vw solid black';
-        
-        document.getElementById("second-sidebar").style.width = "12vw";
+        document.getElementById("second-sidebar").style.width = '12vw';
+        document.getElementById("box").style.margin = '2vw 3.25vw 1.25vw';
     }
 
-}   
+}  
 
