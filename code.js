62515049fddaa087683107078a452d005a885425
Aidan McAra <51344785+aidansbhs@users.noreply.github.com>
2020-08-07 13:36:53 +1200

    Progress on quiz
    Majority of the code is in place with only some last touches to do. Still testing the quiz though.

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ function Nav() {
         document.getElementById("box").style.margin = '2vw 1.25vw 1.25vw';
         CLOSED = false;
         OPEN = true;
-        
+
         document.getElementById("second-sidebar").style.width = "0px";
 
     } else {
@@ -20,18 +20,44 @@ function Nav() {
         document.getElementById("box").style.margin = '2vw 3.25vw 1.25vw';
     }
 
-}  
+}
 
-var quizQuestions = ['What is my favourite color?', 'what is my favourite subject?'];
+var quizQuestions = ['What is my favourite color?', //1
+                     'what is my favourite subject?', //2
+                     'what time is it?']; //3
+
+var answers = [
+    [
+    'Red', //1
+     'green' //2
+    ], // [1][1-2]
+    [
+    'not maths', //1
+    'dtc' //2
+    ], //[2][1-2]
+    [
+    'time', //1
+    'lunch time' //2
+    ] //[3][1-2]
+    ];
 
 
 
-function questions() {
+function loadQuestions() {
     console.log('please work');
-    var randQ = Math.floor(Math.random()*quizQuestions.length);
+    var randQ = Math.floor(Math.random() * quizQuestions.length);
     document.getElementById('questions').innerHTML = quizQuestions[randQ];
+    
+    for(var i=0;i<3;i++){
+        document.getElementById('answer-btn1' +i).innerHTML = answers[randQ][i];
+    }
 }
 
 function check() {
     console.log('yep');
+}
+
+window.onload = function() {
+    loadQuestions();
+
 }
